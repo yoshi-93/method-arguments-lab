2d2cc5721057ca961c2258cc09f11e39614bc406
Mario Lopez <ez0212sp@icloud.com>
2020-12-09 18:15:46 -0500

    commit tree 63fd93aac8f2ceb98f30aad9b1e5f7248fa2b976

---
+++ lib/introduction.rb
@@ -0,0 +1,30 @@
+#
+#
+#def say_hello_x_times(x)
+#    x.times do
+#        puts "Hello World" 
+#    end
+#end
+
+# lib/introduction.rb
+
+# lib/introduction.rb
+# lib/introduction.rb
+
+def introduction(name)
+    puts "Hi, my name is #{name}."
+  end
+#Introducation_with_language
+
+def introduction_with_language(name, language)
+    puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+introduction_with_language("Josh","Ruby")
+introduction_with_language("Dan", "Ember.js.")
+
+
+
+
+
+
+
